a13275f8df4d71ff9bd99227064dddc9648121cc
Tae2u2 <kangnimell@gmail.com>
2025-09-16 23:46:51 +0900

    style: 라이브 요청 리스트 UI 작업

--- src/components/manage/ArtistLiveAsk.tsx
+++ src/components/manage/ArtistLiveAsk.tsx
@@ -5,8 +5,16 @@ const ArtistLiveAsk = () => {
   return (
     <div className="w-full max-w-[550px]">
       <SectionTitle text="Artist Live Ask" />
-      <div className="p-5 shadow ">
-        <p className="mb-3">아티스트 라이브 방송 요청</p>
+      <div className="flex flex-col gap-2 p-5 shadow ">
+        <p className="font-semibold text-sm">라이브 예약 요청</p>
+        <ul className="flex flex-col gap-3 w-full">
+          <li className="w-full px-6 py-2 rounded-full bg-slate-100 shadow">
+            <p className="text-sm">
+              아티스트 김멍멍의 라이브 방송 예약 <br />
+              <span>2025-09-16 20:00</span>
+            </p>
+          </li>
+        </ul>
       </div>
     </div>
   );

48eab9efa8db4d37395c9873b150e93c106f00d7
Tae2u2 <kangnimell@gmail.com>
2025-09-20 21:08:12 +0900

    feat: 화면적용

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -1,9 +1,12 @@
-import AnimalEarsLiveWithSelector from "@/features/live/LiveShowView";
-
 const LivePage = () => {
   return (
-    <div>
-      <AnimalEarsLiveWithSelector />
+    <div className="w-full h-screen">
+      <iframe
+        src="/face-filter/index.html"
+        width="100%"
+        height="900px"
+        className="border-0"
+      />
     </div>
   );
 };

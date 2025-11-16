b43d34240a7a3792bbf2d7fe3ea02bebfb7bf425
Tae2u2 <kangnimell@gmail.com>
2025-11-17 03:43:16 +0900

    feat: 화상 필터 코드 변경

--- public/face-filter/js/config/colorFilterConfig.js
+++
@@ -1,8 +0,0 @@
-// 컬러 필터 설정
-export const colorFilters = [
-  { id: "grayscale", name: "흑백", icon: "⚫" },
-  { id: "sepia", name: "세피아", icon: "🟤" },
-  { id: "warm", name: "따뜻", icon: "🟠" },
-  { id: "cool", name: "차가움", icon: "🔵" },
-  { id: "vintage", name: "빈티지", icon: "📷" },
-];
